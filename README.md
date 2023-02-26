842b145d442b35662200c0569355a6ffb2ebd4f2
Kinori <russel.richard@outlook.com>
2023-02-26 20:13:06 +0000

    added description, usage and credits sections to README. added output folder. changed index.js file to index.mjs

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # TeamProfile-Composer
+
+## Description
+This repo contains a Node.js command-line application which takes information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+
+The application accepts user input to to gather information about the development team members and creates objects for each team member using the correct classes as blueprints.
+
+Technologies used include the inquirer, joi and jest packages, html, css, javascript, 
+
+## Usage 
+1. Right click on the index.js file and select **Open in Integrated Terminal** 
+2. In the opened terminal window, type the command ```index.js``` to start the application.
+3. Enter the **team manager**’s details as prompted:
+      * Name
+      * Employee ID
+      * Email address
+      * Office number
+4. You will then be presented with an option menu for selecting the other team members and/or completing the team build. Use the Up and down keys on your keyboard to make the required selection.
+5. Enter the details as prompted.
+    * The **team engineer**’s:
+      * Name
+      * Employee ID
+      * Email address
+      * GitHub username
+    The **intern**’s:
+      * Name
+      * Employee ID
+      * Email address
+      * School
+6. When you have completed building the team, select **Finish building the team**.
+7. The message **HTML file generated successfully!** will then be displayed indicated that the team build was a success.
+8. A file named `team.html` will then be created in the `output` folder.
+9. Open the `team.html` file to view the newly created team's basic info.
+    * Right click the `team.html` file and select **Open with Live Server** or **Open with Default Browser**.
+10. A screenshot of the generated HTML’s appearance and functionality can be seen below:
+
+![HTML webpage titled “My Team” features five boxes listing employee names, titles, and other key info.](./Assets/14-object-oriented-programming-challenge-demo.png)
+
+
+## Credits
+The resources used for the development of this application are listed below:
+  - https://joi.dev/api/?v=17.8.1
+  - https://www.geeksforgeeks.org/node-js-path-resolve-method/
+  - https://www.npmjs.com/package/inquirer
+  
---
+++ index.mjs
@@ -0,0 +1,240 @@
+import Manager from './lib/Manager.js';
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
+import render from './src/page-template.js';
+import inquirer from 'inquirer';
+import Joi from "joi";
+import path from "path";
+import fs from "fs";
+
+const OUTPUT_DIR = path.resolve("output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const employees = [];
+
+function addManager() {
+    console.log('Please enter the team manager\'s information:');
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name:',
+            validate: function (value) {
+                const mgrNameSchema = Joi.string().trim().min(3).max(30).required();
+                const { error } = mgrNameSchema.validate(value);
+                if (error) {
+                    return "Please enter the team manager's name.";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID:',
+            validate: function (value) {
+                const mgrIdSchema = Joi.number().integer().min(1).max(50).required();
+                const { error } = mgrIdSchema.validate(value);
+                if (error) {
+                    return "Please enter a valid whole number between 1 and 50";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email address:',
+            validate: function (value) {
+                const mgrEmailSchema = Joi.string().email().required();
+                const { error } = mgrEmailSchema.validate(value);
+                if (error) {
+                    return "Please enter a valid email address.";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Office number:',
+            validate: function (value) {
+                const schema = Joi.number().integer().min(1).max(10).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a valid number between 1 and 10";
+                }
+                return true;
+            },
+        },
+    ]).then((response) => {
+        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        employees.push(manager);
+        menu();
+    });
+};
+
+function addEngineer() {
+    console.log('Please enter the engineer\'s information:');
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name:',
+            validate: function (value) {
+                const schema = Joi.string().trim().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a name containing only letters";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID:',
+            validate: function (value) {
+                const schema = Joi.string().trim().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a name containing only letters";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email address:',
+            validate: function (value) {
+                const schema = Joi.string().trim().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a name containing only letters";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'GitHub username:',
+            validate: function (value) {
+                const schema = Joi.string().trim().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a name containing only letters";
+                }
+                return true;
+            },
+        },
+    ]).then((response) => {
+        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+        employees.push(engineer);
+        menu();
+    });
+};
+
+function addIntern() {
+    console.log('Please enter the intern\'s information:');
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name:',
+            validate: function (value) {
+                const schema = Joi.string().trim().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a name containing only letters";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID:',
+            validate: function (value) {
+                const schema = Joi.string().number().integer().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a number between 1 and 50";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Email address:',
+            validate: function (value) {
+                const schema = Joi.string().email().required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a name containing only letters";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'School:',
+            validate: function (value) {
+                const schema = Joi.string().trim().min(3).max(30).required();
+                const { error } = schema.validate(value);
+                if (error) {
+                    return "Please enter a school containing only letters";
+                }
+                return true;
+            },
+        },
+    ]).then((response) => {
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        employees.push(intern);
+        menu();
+    });
+};
+
+function menu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                'Add an engineer',
+                'Add an intern',
+                'Finish building the team',
+            ],
+        },
+    ]).then((response) => {
+        switch (response.choice) {
+            case 'Add an engineer':
+                addEngineer();
+                break;
+            case 'Add an intern':
+                addIntern();
+                break;
+            default:
+                // Finish building the team
+                const html = render(employees);
+                fs.writeFile(outputPath, html, err => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log('HTML file generated successfully!');
+                    }
+                });
+                break;
+        }
+    });
+};
+
+addManager();
